15c09a6fc1fbc8a9fef4bf34231f34030b43c151
Azeri-ne <alterna811@gmail.com>
2024-05-10 07:32:04 +0800

    put populating frames into a function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <chrono>
-#include <thread>
 #include <Windows.h>
 #include <mmsystem.h>
 #include <opencv2/opencv.hpp>
@@ -18,10 +16,12 @@ void checkForOpenCV()
     {
         std::cout << "OpenCV is detected." << '\n';
     }
+    Sleep(1500);
 }
 
-cv::VideoCapture openVid(std::string &filePath)
+cv::VideoCapture openVid()
 {
+    std::string filePath{R"(C:\Users\Harold.DESKTOP-UJ6F4M4\Videos\video.mp4)"};
     cv::VideoCapture bapple{filePath};
     if(!bapple.isOpened())
     {
@@ -32,8 +32,27 @@ cv::VideoCapture openVid(std::string &filePath)
     {
         std::cout << "File is accessed." << '\n';
     }
+    Sleep(1500);
 
     return bapple;
+}
+
+void showFileInfo(cv::VideoCapture bapple, int width, int height)
+{
+    std::cout << "Resolution: " << width << " x " << height << '\n';
+    Sleep(1500);
+}
+
+void preloadFrame(cv::VideoCapture bapple, std::vector<cv::Mat> &totalFrames, cv::Mat &frame)
+{
+    std::cout << "Creating empty frame..." << '\n';
+    while(bapple.read(frame))
+    {
+        totalFrames.push_back(frame.clone());
+    }
+    bapple.release();
+    std::cout << "Empty frame is created!" << '\n';
+    Sleep(1500);
 }
 
 char brightness2Ascii(int brightness, std::string asciiCharset)
@@ -42,48 +61,79 @@ char brightness2Ascii(int brightness, std::string asciiCharset)
     return asciiCharset[index];
 }
 
-int main()
+void populateFrame(cv::Mat &frame, cv::Mat &grayFrame, std::vector<cv::Mat> &totalFrames, std::vector<std::string> &asciiFrames, int completeFrameAmount, int frameIndex, int width, int height, int aspectRatio)
 {
-    checkForOpenCV();
+    std::cout << "Populating empty frame with strings..." << '\n';
+    Sleep(1500);
 
-    std::string filePath{R"(C:\Users\Harold.DESKTOP-UJ6F4M4\Videos\video.mp4)"};
-    cv::VideoCapture bapple{openVid(filePath)};
-
-    double fps{bapple.get(cv::CAP_PROP_FPS)};
-    std::cout << fps << '\n';
-
-    std::vector<cv::Mat> totalFrames{}; // preloadVidFrames();
-    cv::Mat frame{};
-
-    while(bapple.read(frame))
-    {
-        int reductionFactor{12};
-        cv::Mat resizedFrame{};
-        cv::resize(frame, resizedFrame, cv::Size(480 / reductionFactor, 360 / reductionFactor));
-
-        totalFrames.push_back(frame.clone());
-    }
-    bapple.release();
-
-    for(const auto &frame:totalFrames)
+    for(const auto &frame : totalFrames)
     {
         std::string asciiArt{};
-        for(int i{0}; i < frame.rows; i++)
+
+        cv::cvtColor(frame, grayFrame, cv::COLOR_BGR2GRAY);
+        cv::resize(grayFrame, grayFrame, cv::Size(2 * width / aspectRatio, height / aspectRatio), 0, 0);
+
+        asciiArt.clear();
+
+        for(int i{0}; i < grayFrame.rows; i++)
         {
-            for(int j{0}; j < frame.cols; j++)
+            for(int j{0}; j < grayFrame.cols; j++)
             {
-                const std::string asciiCharset{" .'-=*%#@"};
-                int brightness{frame.at<uchar>(i, j)};
+                const std::string asciiCharset{".-=*%#@"};
+                int brightness{grayFrame.at<uchar>(i, j)};
                 char pixelChar{brightness2Ascii(brightness, asciiCharset)};
                 asciiArt += pixelChar;
             }
             asciiArt += '\n';
         }
-        // system("cls");
-        std::cout << asciiArt;
-        cv::waitKey(fps);
+        asciiFrames.push_back(asciiArt);
+        frameIndex++;
 
+
+        std::cout << frameIndex << " out of " << completeFrameAmount << " frames processed." << std::flush << '\n';
     }
+
+    std::cout << "Population done!" << '\n';
+}
+
+void playAscii(std::vector<std::string> &asciiFrames)
+{
+    PlaySound(TEXT(R"(C:\Users\Harold.DESKTOP-UJ6F4M4\Music\bad_apple.wav)"), NULL, SND_FILENAME | SND_ASYNC);
+    for(int i{0}; i < asciiFrames.size(); i++)
+    {
+        std::cout << asciiFrames[i] << std::flush;
+        cv::waitKey(29);
+    }
+}
+
+int main()
+{
+    checkForOpenCV();
+
+    cv::VideoCapture bapple{openVid()};
+
+    int width(bapple.get(cv::CAP_PROP_FRAME_WIDTH));
+    int height(bapple.get(cv::CAP_PROP_FRAME_HEIGHT));
+
+    bapple.get(cv::CAP_PROP_FPS);
+
+    showFileInfo(bapple, width, height);
+
+    std::vector<cv::Mat> totalFrames{};
+    cv::Mat frame{};
+    preloadFrame(bapple, totalFrames, frame);
+
+    int aspectRatio{12};
+    cv::Mat grayFrame{};
+    std::vector<std::string> asciiFrames{};
+    int completeFrameAmount(totalFrames.size());
+    int frameIndex{0};
+
+    populateFrame(frame, grayFrame, totalFrames, asciiFrames, completeFrameAmount, frameIndex, width, height, aspectRatio);
+
+    system("pause");
+
+    playAscii(asciiFrames);
 
     return 0;
 }
